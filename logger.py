2ef22c72496f0e916001f023100313d3831dd9ae
KazeroG <17680194+KazeroG@users.noreply.github.com>
2023-06-06 19:57:00 -0400

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,5 +1,16 @@
-# logger.py
 import logging
 
-logging.basicConfig(level=logging.INFO)
+# Create a logger instance
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Create a file handler and set its level
+file_handler = logging.FileHandler('app.log')
+file_handler.setLevel(logging.INFO)
+
+# Create a formatter and add it to the file handler
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+
+# Add the file handler to the logger
+logger.addHandler(file_handler)
