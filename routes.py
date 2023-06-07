d3618e5caab776a17bd1d503cabddac7de96de4f
KazeroG <17680194+KazeroG@users.noreply.github.com>
2023-06-06 20:01:19 -0400

    enhancement

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask import request, make_response, jsonify
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
-from app import app, db, bcrypt, logger
+from app import app, db, bcrypt
 from models import User
 from errors import *
 from langchain.llms import OpenAI
@@ -9,66 +9,76 @@ from langchain.vectorstores import Chroma
 from langchain.agents.agent_toolkits import (
     create_vectorstore_agent,
     VectorStoreToolkit,
-    VectorStoreInfo
+    VectorStoreInfo,
 )
 
 # Create instance of OpenAI LLM
 llm = OpenAI(temperature=0.1, verbose=True)
 
-# Define process_prompt function
+
 def process_prompt(prompt):
     # Process the prompt and return a response and search results
     # This is a placeholder implementation
     response = prompt
-    search_results = ''
+    search_results = ""
     return response, search_results
 
+
 def register_routes(app):
-    @app.route('/register', methods=['POST'])
+    @app.route("/register", methods=["POST"])
     def register():
         try:
-            username = request.json.get('username')
-            email = request.json.get('email')
-            password = request.json.get('password')
+            username = request.json.get("username")
+            email = request.json.get("email")
+            password = request.json.get("password")
 
             if not username or not email or not password:
-                logger.error('Please provide username, email, and password')
-                return make_response(jsonify({"error": "Please provide username, email, and password"}), 400)
+                logger.error("Please provide username, email, and password")
+                return make_response(
+                    jsonify({"error": "Please provide username, email, and password"}),
+                    400,
+                )
 
-            hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+            hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
 
             new_user = User(username=username, email=email, password=hashed_password)
             db.session.add(new_user)
             db.session.commit()
 
-            logger.info('User registered successfully')
+            logger.info("User registered successfully")
             return jsonify({"message": "User registered successfully"}), 201
 
         except Exception as e:
             db.session.rollback()
-            logger.exception('An error occurred while registering user')
-            return make_response(jsonify({"error": "An error occurred while registering user"}), 500)
+            logger.exception("An error occurred while registering user")
+            return make_response(
+                jsonify({"error": "An error occurred while registering user"}), 500
+            )
 
-    @app.route('/login', methods=['POST'])
+    @app.route("/login", methods=["POST"])
     def login():
         try:
-            email = request.json.get('email')
-            password = request.json.get('password')
+            email = request.json.get("email")
+            password = request.json.get("password")
 
             user = User.query.filter_by(email=email).first()
 
             if not user or not bcrypt.check_password_hash(user.password, password):
-                logger.error('Invalid email or password')
-                return make_response(jsonify({"error": "Invalid email or password"}), 401)
+                logger.error("Invalid email or password")
+                return make_response(
+                    jsonify({"error": "Invalid email or password"}), 401
+                )
 
             access_token = create_access_token(identity=user.id)
             return jsonify({"access_token": access_token}), 200
 
         except Exception as e:
-            logger.exception('An error occurred while logging in')
-            return make_response(jsonify({"error": "An error occurred while logging in"}), 500)
+            logger.exception("An error occurred while logging in")
+            return make_response(
+                jsonify({"error": "An error occurred while logging in"}), 500
+            )
 
-    @app.route('/protected', methods=['GET'])
+    @app.route("/protected", methods=["GET"])
     @jwt_required()  # Authentication required
     def protected():
         try:
@@ -76,49 +86,59 @@ def register_routes(app):
             return jsonify({"user_id": current_user}), 200
 
         except Exception as e:
-            logger.exception('An error occurred while accessing protected route')
-            return make_response(jsonify({"error": "An error occurred while accessing protected route"}), 500)
+            logger.exception("An error occurred while accessing protected route")
+            return make_response(
+                jsonify({"error": "An error occurred while accessing protected route"}),
+                500,
+            )
 
-    @app.route('/users', methods=['GET'])
+    @app.route("/users", methods=["GET"])
     def get_users():
         try:
             users = User.query.all()
-            user_list = [{"id": user.id, "username": user.username, "email": user.email} for user in users]
+            user_list = [
+                {"id": user.id, "username": user.username, "email": user.email}
+                for user in users
+            ]
             return jsonify(user_list)
 
         except Exception as e:
-            logger.exception('An error occurred while fetching users')
-            return make_response(jsonify({"error": "An error occurred while fetching users"}), 500)
+            logger.exception("An error occurred while fetching users")
+            return make_response(
+                jsonify({"error": "An error occurred while fetching users"}), 500
+            )
 
-    @app.route('/users/<user_id>', methods=['DELETE'])
+    @app.route("/users/<user_id>", methods=["DELETE"])
     def delete_user(user_id):
         try:
             user = User.query.get(user_id)
             if not user:
-                logger.error(f'User not found: {user_id}')
+                logger.error(f"User not found: {user_id}")
                 return make_response(jsonify({"error": "User not found"}), 404)
 
             db.session.delete(user)
             db.session.commit()
 
-            logger.info('User deleted successfully')
+            logger.info("User deleted successfully")
             return jsonify({"message": "User deleted successfully"}), 200
 
         except Exception as e:
             db.session.rollback()
-            logger.exception('An error occurred while deleting user')
-            return make_response(jsonify({"error": "An error occurred while deleting user"}), 500)
+            logger.exception("An error occurred while deleting user")
+            return make_response(
+                jsonify({"error": "An error occurred while deleting user"}), 500
+            )
 
-    @app.route('/process_prompt', methods=['POST'])
-    def process():
-        try:
-            prompt = request.json['prompt']
-            response, search_results = process_prompt(prompt)
-            return jsonify({
-                'response': response,
-                'search_results': search_results
-            })
 
-        except Exception as e:
-            logger.exception('An error occurred while processing prompt')
-            return make_response(jsonify({"error": "An error occurred while processing prompt"}), 500)
+@app.route("/process_prompt", methods=["POST"])
+def process():
+    try:
+        prompt = request.json["prompt"]
+        response, search_results = process_prompt(prompt)
+        return jsonify({"response": response, "search_results": search_results})
+
+    except Exception as e:
+        app.logger.exception("An error occurred while processing prompt")
+        return make_response(
+            jsonify({"error": "An error occurred while processing prompt"}), 500
+        )
